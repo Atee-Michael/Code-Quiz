c7d17ed589d46ad7b9d4bcec4f49fed5d82cc741
Michael Atee <ateemichael@yahoo.com>
2023-03-17 22:37:07 +0000

    added sample questions and answers to code for a sanity check

---
+++ assets/JS/questions.js
@@ -0,0 +1,27 @@
+// Variable to store questions
+let questions = [
+    {
+        question: "In the JavaScript, which one of the following is not considered as an error?",
+        answers: ["Missing of semicolons", "Syntax error", "Division by zero", "Missing of Bracket"],
+        correctAnswer: 2
+      },
+      {
+        question: "In JavaScript the x===y statement implies that____?",
+        answers: ["Both are equal in the value and data type", "Both are not same at all", "Both are x and y are equal in value only", "Both x and y are equal in value, type and reference address as well"],
+        correctAnswer: 0
+      },
+      {
+        question: "What is the smallest planet in our solar system?",
+        answers: ["Mercury", "Venus", "Mars"],
+        correctAnswer: 0
+      }
+
+
+
+
+
+
+
+
+    // Add more questions here 
+]
