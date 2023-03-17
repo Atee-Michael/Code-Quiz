4dde576d606aca5fbd0c2e55c909106b7fd8780c
Michael Atee <ateemichael@yahoo.com>
2023-03-17 23:16:29 +0000

    added more questions

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -11,17 +11,44 @@ let questions = [
         correctAnswer: 0
       },
       {
-        question: "What is the smallest planet in our solar system?",
-        answers: ["Mercury", "Venus", "Mars"],
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above",],
         correctAnswer: 0
-      }
-
-
-
-
-
-
-
-
+      },
+      {
+        question: " Which one of the following operator is used to check weather a specific property exists or not______",
+        answers: ["in", "exist", "Exists", "within"],
+        correctAnswer: 0
+      },
+      {
+        question: "What if we define a “for” loop and it removes one of the properties that has not yet been enumerated?",
+        answers: ["The loop will not run at all", "The removed property will be stored in a cache", "That property will be enumerated", "That specific property will not be enumerated"],
+        correctAnswer: 3
+      },
+      {
+        question: "Which one of the following is not a keyword_____",
+        answers: ["with","if", "debugger", "use strict"],
+        correctAnswer: 3
+      },
+      {
+        question: "Which one of the following is not considered as “statement” in the JavaScript?",
+        answers: ["with", "if", "use strict", "debugger"],
+        correctAnswer: 2
+      },
+      {
+        question: " In JavaScript, what is a block of statement?",
+        answers: ["block that contains a single statement", "block that combines a number of statements into a single compound statement", "Conditional block", "both conditional block and a single statement"],
+        correctAnswer: 1
+      },
+      {
+        question: "Which one of the following also known as Conditional Expression____",
+        answers: ["Switch statement", "Alternative to if-else", "If-then-else statement", "immediate if"],
+        correctAnswer: 3
+      },
+      {
+        question: "Which one of the following operator returns false if both values are equal?",
+        answers: ["==", "!==", "!", "&&"],
+        correctAnswer: 0
+      },
     // Add more questions here 
 ]
